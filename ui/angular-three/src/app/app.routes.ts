7dae26dca899b8c4b28b157088acbf50674cd45c
Linda Wang <61102300+ylw311@users.noreply.github.com>
2023-11-25 01:24:11 -0500

    homepage incomplete

--- ui/angular-three/src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { CubeComponent } from './cube/cube.component';
-
-export const routes: Routes = [{ path: 'cube', component: CubeComponent },];
--- ui/cra-threejs/src/App.js
+++ ui/cra-threejs/src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import Three from './Three';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from './components/main.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <Three />
-
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          {/* <Three /> */}
+        </header>
+        
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
