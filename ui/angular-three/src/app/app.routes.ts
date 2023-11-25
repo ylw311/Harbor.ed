e974160cf883d8dafce78128de56b0d797ab54bd
Linda Wang <61102300+ylw311@users.noreply.github.com>
2023-11-24 23:46:04 -0500

    uhhhh setups

--- ui/angular-three/src/app/app.routes.ts
+++ ui/angular-three/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { CubeComponent } from './cube/cube.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'cube', component: CubeComponent },];
