12ba4b98fb3463e6b3a2e3edf99c989fdc94dcb4
Linda Wang <61102300+ylw311@users.noreply.github.com>
2023-11-24 23:18:55 -0500

    init

--- ui/angular-three/README.md
+++ ui/angular-three/README.md
@@ -1,4 +1,4 @@
-# Angular
+# AngularThree
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.3.
 
---
+++ ui/angular-three/src/app/cube/cube.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CubeComponent } from './cube.component';
+
+describe('CubeComponent', () => {
+  let component: CubeComponent;
+  let fixture: ComponentFixture<CubeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CubeComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CubeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
